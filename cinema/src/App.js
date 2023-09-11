a19a2d3b86ad6235f55f31ef9bfb2e0a2ec292a1
mailet01 <111491293+mailet01@users.noreply.github.com>
2023-09-11 12:15:08 +0200

    data från jsonfilen

---
+++ cinema/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client'
+
+import './App.css';
+function App() {
+    const [cinemaData, setCinemaData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/cinema.json');
+                if (!response.ok) {
+                    throw new error('there was not ok');
+                }
+                const data = await response.json();
+            console.log("data", data);
+            const maped = cinemaData.map(cinemaData);
+            
+                setCinemaData(data);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(true)
+            }
+
+        };
+        fetchData();
+    }
+    );
+    if (loading) {
+        return <div>loading...</div>
+    }
+    if (error) {
+        return <div>error: {error.message}</div>
+    }
+
+
+
+    return (
+        <main>
+            <h1 className='cinema'>{cinemaData.cinema.name}</h1>
+        </main>
+
+    )
+}
+
+export default App;
